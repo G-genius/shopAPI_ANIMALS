df3debcdb797756858160b75cd389d0617bcb52a
G-genius <gamelob25@gmail.com>
2022-04-22 11:28:34 +0300

    add js for registration

---
+++ Интернет магазин ShopApi/src/js/registration.js
@@ -0,0 +1,13 @@
+let email = document.querySelector('.auth_email');
+let pass = document.querySelector('.auth_pass')
+async function main() {
+	await registr();
+}
+
+async function registr() {
+	if (email != null && pass != null) {
+		alert("Registr");
+	}
+	alert("Not regist");
+	
+}
